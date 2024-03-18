654db10dc15a93d8f136640b73995fd5495bd51f
alberterc <85069947+alberterc@users.noreply.github.com>
2024-03-18 15:48:28 +0700

    Removed missing dependencies

--- lib/router.dart
+++ lib/router.dart
@@ -1,8 +1,6 @@
-import 'package:flame/game.dart';
 import 'package:go_router/go_router.dart';
 
 import 'widgets/screen_title.dart';
-import 'puzzle_game/dialog_screen.dart';
 import 'widgets/screen_main_menu.dart';
 import 'widgets/screen_settings.dart';
 
@@ -15,10 +13,6 @@ final router = GoRouter(
         GoRoute(
           path: 'main_menu',
           builder: (context, state) => const ScreenMainMenu(),
-        ),
-        GoRoute(
-          path: 'game',
-          builder: (context, state) => GameWidget(game: DialogScreen())
         ),
         GoRoute(
           path: 'settings',
