b437f60346240186581dc4579b46eda29855387f
alberterc <alberterc.ps@gmail.com>
2024-04-08 19:59:45 +0700

    feat: add onTapUp event
    - also removed unnecessary code

--- lib/game/components/levelComponents/level_puzzle.dart
+++ lib/game/components/levelComponents/level_puzzle.dart
@@ -1,13 +1,15 @@
 import 'dart:async';
 
+import 'package:flame/events.dart';
 import 'package:flutter/material.dart';
 import 'package:flame/components.dart';
 
 import '../../screen_game.dart';
 
-class LevelPuzzle extends PositionComponent with HasGameReference<ScreenGame> {
-  Vector2 get puzzleBoxSize => boxSize;
-  set puzzleBoxSize(Vector2 puzzleBoxSize) => boxSize = puzzleBoxSize;
+class LevelPuzzle extends PositionComponent with HasGameReference<ScreenGame>, TapCallbacks {
+  LevelPuzzle({
+    required this.onTapUpEvent
+  });
 
   Color get puzzleBoxColor => boxColor;
   set puzzleBoxColor(Color puzzleBoxColor) => boxColor = puzzleBoxColor;
@@ -15,13 +17,9 @@ class LevelPuzzle extends PositionComponent with HasGameReference<ScreenGame> {
   String get puzzleBoxText => boxText;
   set puzzleBoxText(String puzzleBoxText) => boxText = puzzleBoxText;
 
-  Anchor get puzzleBoxAnchor => boxAnchor;
-  set puzzleBoxAnchor(Anchor puzzleBoxAnchor) => boxAnchor = puzzleBoxAnchor;
-
-  Vector2 boxSize = Vector2(20, 20);
   Color boxColor = const Color.fromARGB(255, 255, 255, 255);
   String boxText = '';
-  Anchor boxAnchor = Anchor.topLeft;
+  final Function(TapUpEvent, String) onTapUpEvent;
 
   late TextBoxComponent text;
   late TextPaint textPaint;
@@ -40,34 +38,41 @@ class LevelPuzzle extends PositionComponent with HasGameReference<ScreenGame> {
     text = TextBoxComponent(
       text: boxText,
       textRenderer: textPaint,
-      anchor: boxAnchor,
       align: Anchor.center,
-      size: boxSize,
+      size: size,
       boxConfig: TextBoxConfig(
-        maxWidth: boxSize.x,
+        maxWidth: size.x,
         margins: EdgeInsets.zero
       )
     );
 
-    box = SpriteComponent(
-      sprite: Sprite(
-        game.images.fromCache('background.png')
-      ),
-      size: boxSize,
-      paint: Paint()..color = boxColor,
-      anchor: boxAnchor
-    );
+    // box = SpriteComponent(
+    //   sprite: Sprite(
+    //     game.images.fromCache('background.png')
+    //   ),
+    //   size: size,
+    //   paint: Paint()..color = boxColor
+    // );
 
-    addAll([box, text]);
+    addAll([
+      // box,
+      text
+    ]);
 
     return super.onLoad();
   }
 
-  // @override
-  // void render(Canvas canvas) {
-  //   canvas.drawRect(
-  //     Rect.fromLTWH(0, 0, boxSize.x, boxSize.y),
-  //     Paint()..color = boxColor
-  //   );
-  // }
+  @override
+  void onTapUp(TapUpEvent event) {
+    super.onTapUp(event);
+    onTapUpEvent(event, boxText);
+  }
+
+  @override
+  void render(Canvas canvas) {
+    canvas.drawRect(
+      Rect.fromLTWH(0, 0, size.x, size.y),
+      Paint()..color = boxColor
+    );
+  }
 }
