4aea6c44c726baef326df6d93b005fd8c3422f2e
alberterc <alberterc.ps@gmail.com>
2024-04-08 19:50:25 +0700

    feat: add library for constant values
    - deleted utils.dart and moved values into constants library

---
+++ lib/constants/constants.dart
@@ -0,0 +1,8 @@
+const String gameName = "Pedjoeang Indonesia";
+const Map<String, int> renderPriority = {
+  'ui': 10,
+  'under_ui': 9,
+  'foreground': 1,
+  'background': 0
+};
+const double characterSize = 320.0;
