26d94b8806071c916bbb8916450617fc7f286308
alberterc <alberterc.ps@gmail.com>
2024-04-08 20:00:15 +0700

    chore: remove unnecessary code

--- lib/game/components/levelComponents/level_table.dart
+++ lib/game/components/levelComponents/level_table.dart
@@ -4,15 +4,10 @@ import 'package:flame/components.dart';
 import '../../screen_game.dart';
 
 class LevelTable extends PositionComponent with HasGameReference<ScreenGame> {
-  Vector2 get tableBoxSize => boxSize;
-  set tableBoxSize(Vector2 tableBoxSize) => boxSize = tableBoxSize;
-
-  Vector2 boxSize = Vector2(20, 20);
-
   @override
   void render(Canvas canvas) {
     canvas.drawRect(
-      Rect.fromLTWH(0, 0, boxSize.x, boxSize.y),
+      Rect.fromLTWH(0, 0, size.x, size.y),
       Paint()..color = game.palette.backgroundSecondary.color
     );
   }
