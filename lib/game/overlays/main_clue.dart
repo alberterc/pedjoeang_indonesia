5eebd1bbf7a9ce78a1ae543a4e004534d2e164e9
Aaron Keane <aaron.wijaya001@binus.ac.id>
2024-05-07 15:11:24 +0700

    feat: add new clue for pigpenCipher and mainPuzzle

--- lib/game/overlays/main_clue.dart
+++ lib/game/overlays/main_clue.dart
@@ -22,9 +22,9 @@ class MainClue {
           color: const Color.fromARGB(117, 0, 0, 0),
           child: Center(
             child: Container(
-              padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.fromLTRB(24.0, 8.0, 0, 8.0), 
               height: screenHeight * 0.6,
-              width: screenWidth * 0.3,
+              width: screenWidth * 0.5,
               decoration: const BoxDecoration(
                 image: DecorationImage(
                   image: AssetImage('assets/images/old_paper.png'),
@@ -36,13 +36,32 @@ class MainClue {
                 builder: (context, value, _) {
                   return Center(
                     child: value['MainPuzzle']! ? Wrap(
-                      spacing: 8.0,
-                      runSpacing: 8.0,
                       crossAxisAlignment: WrapCrossAlignment.center,
                       alignment: WrapAlignment.center,
                       children: [
-                        for (String coord in mainPuzzleShuffledSolutionCoords)
-                          Text(coord)
+                        Container(
+                          alignment: Alignment.center,
+                          height: screenHeight * 0.5,
+                          width: screenWidth * 0.2,
+                          child: Wrap(
+                            spacing: 8.0,
+                            runSpacing: 8.0,
+                            children: [
+                              for (String coord in mainPuzzleShuffledSolutionCoords)
+                                Text(coord)
+                            ],
+                          ),
+                        ),
+                        Container(
+                          height: screenHeight * 0.5,
+                          width: screenWidth * 0.2,
+                          // color: Colors.blue,
+                          decoration: const BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage('assets/images/game/main_clue.png'), 
+                            )
+                          ),
+                        )
                       ],
                     ) : const Text(
                       'Petunjuk Utama Tidak Tersedia',
